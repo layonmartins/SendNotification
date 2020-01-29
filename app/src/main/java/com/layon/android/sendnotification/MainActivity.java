6b6809503a9fdffbe5550dedc513a26bcd98fa71
Layon Martins <layonmartins03@gmail.com>
2020-01-29 14:16:34 -0300

    create a resultActivity to start an activity when user click in the notification

--- app/src/main/java/com/layon/android/sendnotification/MainActivity.java
+++ app/src/main/java/com/layon/android/sendnotification/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.graphics.Color;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -75,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendNotification(View v) {
         CheckBox checkBox_postDelayed = (CheckBox) findViewById(R.id.checkBox_postDelay);
+        CheckBox checkBox_StartActivity = (CheckBox) findViewById(R.id.checkBox_StartActivity);
         Spinner spinner_seconds = (Spinner) findViewById(R.id.spinner_seconds);
 
         //it doesn't work
@@ -82,9 +85,44 @@ public class MainActivity extends AppCompatActivity {
 
         if (checkBox_postDelayed.isChecked()){
             sendNotificationDelayed(v, delay);
+        } else if (checkBox_StartActivity.isChecked()){
+            sendStartActivityNotification();
         } else {
             sendBaseNotification();
         }
+    }
+
+    public void sendStartActivityNotification() {
+
+        //create channel
+        createNotificationChannel();
+
+        //create notification
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.venturus)
+                        .setContentTitle("Layonf PendingIntent Notification Test")
+                        .setContentText(getString(R.string.notifi_text))
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(getString(R.string.notifi_text)))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        //create the pendingIntent:
+        //create an Intent for the activity you want to start
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        //create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        //get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //notify
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+
     }
 }
 
---
+++ app/src/main/java/com/layon/android/sendnotification/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.layon.android.sendnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
